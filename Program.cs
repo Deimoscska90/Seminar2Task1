f8876b02a36c3c81c6b0ac55c6e82542639d8312
Filipp <Filipp_nikolaev@rambler.ru>
2023-01-31 11:55:09 +0600

    Task2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int GetNumber()
+{
+    Random rnd = new Random();
+    int result = rnd.Next(100, 1000);
+    return result;
+}
+
+int GetDeleteOfFirstAndThirdNumber(int number)
+{   
+int SecondDigit = number%100; 
+   
+return SecondDigit/10;
+}
+
+int number = GetNumber();
+int result = GetDeleteOfFirstAndThirdNumber(number);
+
+Console.WriteLine($"Было:{number}, Стало:{result}");
